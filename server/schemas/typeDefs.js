8e41a9197cdd8a64237034138c64fdfd43f4e4a0
PintoDrop <mikebender22@gmail.com>
2023-04-27 10:44:04 -0700

    working on typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const {gql} = require("apollo-server-express");
+
+const typeDefs = gql 
+`
+type User {
+  _id: ID!
+  name: String!
+  email: String!
+  bookcount: Integer!
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String
+  title: String!
+  image: String
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+`
+
+module.exports = typeDefs;
