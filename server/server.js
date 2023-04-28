dd04ba0f4ff2d95e5e3b232514127020e38bd978
PintoDrop <mikebender22@gmail.com>
2023-04-28 08:40:31 -0700

    fixed error in server.js syntax

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const {authMiddleware}= require ('./utils/auth');
 const {typeDefs, resolvers} = require('./schemas');
-// const path = require('path');
+const path = require('path');
 const db = require('./config/connection');
 const auth = require('./utils/auth');
 // const routes = require('./routes');
@@ -18,13 +18,13 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(_dirname, '../client/'));
+  res.sendFile(path.join(__dirname, '../client/'));
 })
 
 // app.use(routes);
@@ -37,7 +37,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
     
-    // console.log(`🌍 Now listening on localhost:${PORT}`));
+
   });
 };
 
