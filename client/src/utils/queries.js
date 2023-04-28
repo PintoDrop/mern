7ea41343af86d96cf90004f72526eb59be02ac46
PintoDrop <mikebender22@gmail.com>
2023-04-27 20:20:57 -0700

    adding utils and finishing resolvers

---
+++ client/src/utils/queries.js
@@ -0,0 +1,1 @@
+// will hold the GET_ME
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,12 @@
 const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if(context.user) {
-        const userData = await.User.findOne({_id: context.user._id})
+        const userData = await User.findOne({_id: context.user._id})
         .select('-__v - password')
         return userData;
       }
@@ -21,9 +22,44 @@ const resolvers = {
         throw new AuthenticationError("Invalid email");
       }
 
+      const validPass = await user.isValidPassword(password);
+
+      if (!validPass) {
+        throw new AuthenticationError("Invalid password");
+      }
       
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signtoken(user);
+
+      return {token, user};
+    },
+
+    saveBook: async (parent, {input}, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          {_id: context.user._id},
+          {$addToSet: {savedBooks: input}},
+          {new:true}
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("Please make sure you are logged in");
+    },
+
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          {_id: context.user._id},
+          {$pull: {savedBooks: {bookId: args.bookId}}},
+          {new: true}
+        );
+        return updatedUser
+      }
+      throw new AuthenticationError("Please make sure you are logged in")
     }
   }
-}
+};
 
 module.exports = resolvers;
